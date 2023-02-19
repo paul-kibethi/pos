8a60af0a5086f77fcde3c4e1ace1a2db87a43f4c
Kibethi Paul <2000kibethipaul@gmail.com>
2023-02-19 19:30:52 +0300

    host change

--- pos/settings.py
+++ pos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--w(dug*va_megpa25w$gmv8o6+f8!fg_*m1^u$)z25pab0hicm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://paulnk.pythonanywhere.com/']
 
 
 # Application definition
@@ -79,9 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pos',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'USER': 'PaulNK',
+        'PASSWORD': '2@Kibepaul',
+        'HOST': 'PaulNK.mysql.pythonanywhere-services.com',
         'PORT': 3306,
     }
 }
